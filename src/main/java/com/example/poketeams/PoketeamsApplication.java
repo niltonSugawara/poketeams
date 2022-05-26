16bda0fad0559e4c5d20c753119b55531a085800
Nilton Mitsuharu Sugawara <nilton.sugawara@gmail.com>
2022-05-25 22:20:18 -0300

    subindo projeto inicial

---
+++ src/main/java/com/example/poketeams/PoketeamsApplication.java
@@ -0,0 +1,13 @@
+package com.example.poketeams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoketeamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoketeamsApplication.class, args);
+	}
+
+}
